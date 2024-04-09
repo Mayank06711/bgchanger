4db5c88ff39c89262691fd598282ea01cfe720b0
Mayank06711 <mayank93soni@gmail.com>
2024-04-09 18:16:53 +0530

    initial setup done

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+  <div className="w-full">hello</div>
+  )
+}
+
+export default App
